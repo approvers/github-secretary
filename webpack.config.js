8bb643f9d5e988b63739917412c765eece9958a4
MikuroXina <ryosukadnak@gmail.com>
2020-05-29 20:39:21 +0900

    Config serverless

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const config = {
   target: 'node',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'api'),
     publicPath: '/',
   },
   node: {
