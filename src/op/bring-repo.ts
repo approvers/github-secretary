891082cb3f4dff1389b90f27c27656a55262557b
MikuroXina <ryosukadnak@gmail.com>
2020-05-30 17:32:09 +0900

    Add bringing repo

---
+++ src/op/bring-repo.ts
@@ -0,0 +1,62 @@
+import { Analecta } from '../exp/analecta';
+import { Message } from 'discord.js';
+import { CommandProcessor, connectProcessors } from '../abst/connector';
+import fetch from 'node-fetch';
+import { replyFailure } from './reply-failure';
+
+const ghPattern = /^\/ghr\s+([^/]+)(\/(.+))?$/;
+
+export const bringRepo = async (analecta: Analecta, msg: Message): Promise<boolean> => {
+  if (!ghPattern.test(msg.content)) {
+    return false;
+  }
+
+  const matches = msg.content.match(ghPattern);
+  if (matches == null) {
+    return false;
+  }
+
+  msg.channel.startTyping();
+  const res = await connectProcessors([
+    internalRepo(matches[1]),
+    externalRepo(matches[1])(matches[3]),
+    replyFailure,
+  ])(analecta, msg);
+  msg.channel.stopTyping();
+  return res;
+};
+
+const externalRepo = (owner: string) => (repo: string): CommandProcessor => async (
+  analecta: Analecta,
+  msg: Message,
+): Promise<boolean> => {
+  const apiUrl = `https://api.github.com/repos/${owner}/${repo}`;
+  const res = await await (await fetch(apiUrl)).json();
+
+  if (!res.url) {
+    return false;
+  }
+
+  const {
+    name,
+    description,
+    html_url,
+    owner: { avatar_url, login },
+  } = res;
+  msg.channel.send({
+    embed: {
+      author: {
+        name: login,
+        icon_url: avatar_url,
+      },
+      url: html_url,
+      description,
+      title: name,
+      footer: { text: analecta.Subscribe },
+    },
+  });
+
+  return true;
+};
+
+const internalRepo = externalRepo('approvers');
--- src/op/index.ts
+++ src/op/index.ts
@@ -1,7 +1,8 @@
 import { bringIssue } from './bring-issue';
 import { flavor } from './flavor';
+import { bringRepo } from './bring-repo';
 import { connectProcessors, CommandProcessor } from '../abst/connector';
 import { Analecta } from '../exp/analecta';
 
 export const procs = (analecta: Analecta): CommandProcessor =>
-  connectProcessors([flavor(new RegExp(analecta.CallPattern)), bringIssue]);
+  connectProcessors([flavor(new RegExp(analecta.CallPattern)), bringIssue, bringRepo]);
