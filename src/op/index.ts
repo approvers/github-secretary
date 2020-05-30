fbb593b4c16bc5816e909147a675cad5c56c81f2
MikuroXina <ryosukadnak@gmail.com>
2020-05-30 21:10:47 +0900

    Add bringing PR

--- src/op/index.ts
+++ src/op/index.ts
@@ -3,6 +3,7 @@ import { flavor } from './flavor';
 import { bringRepo } from './bring-repo';
 import { connectProcessors, CommandProcessor } from '../abst/connector';
 import { Analecta } from '../exp/analecta';
+import { bringPR } from './bring-pr';
 
 export const procs = (analecta: Analecta): CommandProcessor =>
-  connectProcessors([flavor(new RegExp(analecta.CallPattern)), bringIssue, bringRepo]);
+  connectProcessors([flavor(new RegExp(analecta.CallPattern)), bringIssue, bringPR, bringRepo]);
