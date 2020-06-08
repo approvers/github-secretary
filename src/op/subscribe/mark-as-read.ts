2c3337e633f899408d599e646ce5fceff440c112
MikuroXina <ryosukadnak@gmail.com>
2020-06-08 21:12:25 +0900

    Introduce fetchErrorHandler to markAsRead

--- src/op/subscribe/mark-as-read.ts
+++ src/op/subscribe/mark-as-read.ts
@@ -5,6 +5,7 @@ import { CommandProcessor } from 'src/abst/connector';
 import { Analecta } from 'src/exp/analecta';
 import { GitHubUser, DiscordId } from 'src/exp/github-user';
 import { replyFailure } from '../reply-failure';
+import { fetchErrorHandler } from 'src/exp/fetch-error-handler';
 
 export type UserDatabase = {
   fetchUser(discordId: DiscordId): Promise<GitHubUser | undefined>;
@@ -16,30 +17,27 @@ export const markAsRead = (db: UserDatabase): CommandProcessor => async (
   analecta: Analecta,
   msg: Message,
 ): Promise<boolean> => {
-  if (!markPattern.test(msg.content)) {
-    return false;
-  }
-
-  const matches = msg.content.match(markPattern);
+  const matches = markPattern.exec(msg.content);
   if (matches == null) {
     return false;
   }
   const notificationIdToMarkAsRead = matches[1];
-  if (notificationIdToMarkAsRead == null) {
-    return false;
-  }
 
   await msg.channel.startTyping();
 
-  const user = await db.fetchUser(msg.author.id);
+  const user = await db.fetchUser(msg.author.id).catch(
+    fetchErrorHandler(async (embed) => {
+      await msg.reply(embed);
+    }),
+  );
   if (user == null) {
-    msg.channel.stopTyping();
+    msg.channel.stopTyping(true);
     await msg.reply(analecta.NotSubscribed);
     return true;
   }
   const { userName, notificationToken, currentNotificationIds } = user;
   if (!currentNotificationIds.includes(notificationIdToMarkAsRead)) {
-    msg.channel.stopTyping();
+    msg.channel.stopTyping(true);
     return replyFailure(analecta, msg);
   }
 
@@ -52,6 +50,10 @@ export const markAsRead = (db: UserDatabase): CommandProcessor => async (
           `Basic ` + Buffer.from(`${userName}:${notificationToken}`).toString('base64'),
       },
     },
+  ).catch(
+    fetchErrorHandler(async (embed) => {
+      await msg.reply(embed);
+    }),
   );
   msg.channel.stopTyping();
   if (res.status !== 205) {
