b0ab7706f15b6d6f31a09823df7ed7b78c52320e
MikuroXina <ryosukadnak@gmail.com>
2020-06-13 00:13:32 +0900

    Add DiscordMessage

---
+++ src/skin/discord-message.ts
@@ -0,0 +1,25 @@
+import { Message } from 'src/abst/message';
+import { Message as RawMessage, MessageEmbed } from 'discord.js';
+
+export class DiscordMessage implements Message {
+  constructor(private raw: RawMessage) {}
+
+  async match(regex: RegExp): Promise<RegExpMatchArray | null> {
+    return regex.exec(this.raw.content);
+  }
+
+  async withTyping(callee: () => Promise<boolean>): Promise<boolean> {
+    await this.raw.channel.startTyping();
+    const res = await callee();
+    this.raw.channel.stopTyping();
+    return res;
+  }
+
+  async reply(message: string): Promise<void> {
+    await this.raw.reply(message);
+  }
+
+  async sendEmbed(embed: MessageEmbed): Promise<void> {
+    await this.raw.channel.send(embed);
+  }
+}
