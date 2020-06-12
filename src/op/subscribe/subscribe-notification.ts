cac4775aefb17178e4552b5319c9c1b2a2e4f280
MikuroXina <ryosukadnak@gmail.com>
2020-06-13 00:39:44 +0900

    Make subscribeNotification to support our Message

--- src/op/subscribe/subscribe-notification.ts
+++ src/op/subscribe/subscribe-notification.ts
@@ -1,9 +1,8 @@
-import { Message } from 'discord.js';
-
 import { Analecta } from '../../exp/analecta';
 import { CommandProcessor } from '../../abst/connector';
 import { DiscordId, GitHubUser } from '../../exp/github-user';
 import { fetchErrorHandler } from '../../exp/fetch-error-handler';
+import { Message } from '../../abst/message';
 
 export type UserDatabase = {
   register: (id: DiscordId, user: GitHubUser) => Promise<void>;
@@ -19,21 +18,21 @@ export const subscribeNotification = (db: UserDatabase, query: Query): CommandPr
   analecta: Analecta,
   msg: Message,
 ): Promise<boolean> => {
-  const matches = subscribePattern.exec(msg.content);
+  const matches = await msg.matchCommand(subscribePattern);
   if (matches == null || matches[1] == null || matches[2] == null) {
     return false;
   }
 
   const isValidToken = await query.checkNotificationToken(matches[1], matches[2]).catch(
     fetchErrorHandler(async (mes) => {
-      await msg.reply(mes);
+      await msg.sendEmbed(mes);
     }),
   );
   if (!isValidToken) {
     throw new Error('invalid token');
   }
 
-  await db.register(msg.author.id, {
+  await db.register(msg.getAuthorId(), {
     userName: matches[1],
     notificationToken: matches[2],
     currentNotificationIds: [],
