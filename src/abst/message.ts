25cfa776c13cc67a6c1c09c391b0706d2f832054
MikuroXina <ryosukadnak@gmail.com>
2020-06-13 00:12:28 +0900

    Fix definition

--- src/abst/message.ts
+++ src/abst/message.ts
@@ -1,8 +1,8 @@
 import { MessageEmbed } from 'discord.js';
 
 export type Message = {
-  match(regex: RegExp): Promise<RegExpMatchArray>;
-  withTyping(callee: () => Promise<void>): Promise<boolean>;
+  match(regex: RegExp): Promise<RegExpMatchArray | null>;
+  withTyping(callee: () => Promise<boolean>): Promise<boolean>;
   reply(message: string): Promise<void>;
   sendEmbed(embed: MessageEmbed): Promise<void>;
 };
