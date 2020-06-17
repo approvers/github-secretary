c115e522cfaec08c5161b4b560353314a8a2fa68
MikuroXina <ryosukadnak@gmail.com>
2020-06-18 00:45:04 +0900

    Fix lacking await

--- src/op/error.ts
+++ src/op/error.ts
@@ -6,7 +6,7 @@ export const error: CommandProcessor = async (
   analecta: Analecta,
   msg: Message,
 ): Promise<boolean> => {
-  if (!msg.content.startsWith('/gh')) {
+  if ((await msg.matchCommand(/^\/gh/)) == null) {
     return false;
   }
 
