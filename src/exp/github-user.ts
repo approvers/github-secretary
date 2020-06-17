905beef76d3a29694f0b8520893040223d73453c
MikuroXina <ryosukadnak@gmail.com>
2020-06-18 01:35:40 +0900

    Add nominality

--- src/exp/github-user.ts
+++ src/exp/github-user.ts
@@ -1,18 +1,26 @@
-export type NotificationId = string;
+declare const nominalNotificationId: unique symbol;
+export type NotificationId = string & {
+  [nominalNotificationId]: never;
+};
 
+declare const nominalGitHubUser: unique symbol;
 export type GitHubUser = {
   userName: string;
   notificationToken: string;
   currentNotificationIds: NotificationId[];
+  [nominalGitHubUser]: never;
 };
 
-export type DiscordId = string;
+declare const nominalDiscordId: unique symbol;
+export type DiscordId = string & {
+  [nominalDiscordId]: never;
+};
 
 export type GitHubUsers = Record<DiscordId, GitHubUser>;
 
 export const cloneGitHubUsers = (users: GitHubUsers): GitHubUsers => {
-  return Object.entries(users).reduce(
-    (acc, [id, { userName, notificationToken, currentNotificationIds }]) => ({
+  return Object.entries<GitHubUser>(users).reduce(
+    (acc: GitHubUsers, [id, { userName, notificationToken, currentNotificationIds }]) => ({
       ...acc,
       [id]: {
         userName,
