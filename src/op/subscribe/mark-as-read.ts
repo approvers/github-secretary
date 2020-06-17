b09e367f751f3280a9948c8055fe3b6fdc209629
MikuroXina <ryosukadnak@gmail.com>
2020-06-18 03:07:14 +0900

    Fix tests and typings

--- src/op/subscribe/mark-as-read.ts
+++ src/op/subscribe/mark-as-read.ts
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 import { Message, MessageEmbed } from 'discord.js';
 
 import { CommandProcessor } from '../../abst/connector';
@@ -8,10 +7,14 @@ import { DiscordId } from '../../exp/discord-id';
 import { replyFailure } from '../../abst/reply-failure';
 import { fetchErrorHandler } from '../../skin/fetch-error-handler';
 import { Message } from '../../abst/message';
-import { includes } from '../../exp/notifications';
+import { includes, NotificationId } from '../../exp/notifications';
 
 export type UserDatabase = {
   fetchUser(discordId: DiscordId): Promise<GitHubUser | undefined>;
+};
+
+export type Query = {
+  markAsRead(user: GitHubUser, notificationIdToMarkAsRead: NotificationId): Promise<boolean>;
 };
 
 const markPattern = /^\/ghm ([0-9]+)$/;
@@ -24,7 +27,7 @@ export const markAsRead = (db: UserDatabase): CommandProcessor => async (
   if (matches == null) {
     return false;
   }
-  const notificationIdToMarkAsRead = matches[1];
+  const notificationIdToMarkAsRead = matches[1] as NotificationId;
 
   return msg
     .withTyping(async () => {
