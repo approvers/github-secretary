3d734b70e0d5a8c297b952128ac1c4cfde337665
MikuroXina <ryosukadnak@gmail.com>
2020-06-19 02:11:58 +0900

    Add types

--- src/op/subscribe/mark-as-read.test.ts
+++ src/op/subscribe/mark-as-read.test.ts
@@ -1,24 +1,27 @@
 import { markAsRead } from './mark-as-read';
 import { MockMessage } from '../../skin/mock-message';
 import { analectaForTest } from '../../skin/test-analecta';
+import { GitHubUser } from '../../exp/github-user';
+import { DiscordId } from '../../exp/discord-id';
+import { NotificationId } from '../../exp/github-notification';
 
 test('mark a notification as read', async (done) => {
   const analecta = await analectaForTest();
 
   const proc = markAsRead(
     {
-      fetchUser: async (id) => {
+      fetchUser: async (id: DiscordId) => {
         expect(id).toStrictEqual('alice_discord');
 
-        return {
+        return ({
           userName: 'Alice',
           notificationToken: 'TEST_TOKEN',
-          currentNotificationIds: ['0123456789'],
-        };
+          currentNotificationIds: ['0123456789' as NotificationId],
+        } as unknown) as GitHubUser;
       },
     },
     {
-      markAsRead: async (_user, id) => {
+      markAsRead: async (_user: GitHubUser, id: NotificationId) => {
         expect(id).toStrictEqual('0123456789');
         done();
         return true;
@@ -26,6 +29,6 @@ test('mark a notification as read', async (done) => {
     },
   );
 
-  const message = new MockMessage('/ghm 0123456789', 'alice_discord');
+  const message = new MockMessage('/ghm 0123456789', 'alice_discord' as DiscordId);
   expect(proc(analecta, message)).resolves.toEqual(true);
 });
