86e831121b143765d3fbbd95cc25d67170901c31
MikuroXina <ryosukadnak@gmail.com>
2020-06-19 02:14:12 +0900

    Add casting

--- src/op/subscribe/unsubscribe-notification.test.ts
+++ src/op/subscribe/unsubscribe-notification.test.ts
@@ -1,6 +1,7 @@
 import { unsubscribeNotification } from './unsubscribe-notification';
 import { MockMessage } from '../../skin/mock-message';
 import { analectaForTest } from '../../skin/test-analecta';
+import { DiscordId } from '../../exp/discord-id';
 
 test('subscribe a member', async (done) => {
   const analecta = await analectaForTest();
@@ -13,6 +14,6 @@ test('subscribe a member', async (done) => {
     },
   });
 
-  const message = new MockMessage('/ghu', 'alice_discord');
+  const message = new MockMessage('/ghu', 'alice_discord' as DiscordId);
   expect(proc(analecta, message)).resolves.toEqual(true);
 });
