76350ffc1ad0a0a31ca9c846a3302f6c9598a1bd
MikuroXina <ryosukadnak@gmail.com>
2020-06-19 02:11:39 +0900

    Remove unused

--- src/exp/notifications.ts
+++
@@ -1,7 +0,0 @@
-declare const nominalNotificationId: unique symbol;
-export type NotificationId = string & {
-  [nominalNotificationId]: never;
-};
-
-export const includes = (notifications: NotificationId[], target: string): boolean =>
-  (notifications as string[]).includes(target);
