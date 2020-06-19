20d5595d2e34300ab1b932208f3f60e78a562f7a
MikuroXina <ryosukadnak@gmail.com>
2020-06-19 10:34:31 +0900

    Fix importing in Message

--- src/abst/message.ts
+++ src/abst/message.ts
@@ -1,4 +1,5 @@
 import { DiscordId } from '../exp/discord-id.ts';
+import { EmbedMessage } from '../exp/embed-message.ts';
 
 export type Message = {
   getAuthorId(): DiscordId;
@@ -6,5 +7,5 @@ export type Message = {
   matchCommand(regex: RegExp): Promise<RegExpMatchArray | null>;
   withTyping(callee: () => Promise<boolean>): Promise<boolean>;
   reply(message: string): Promise<void>;
-  sendEmbed(embed: MessageEmbed): Promise<void>;
+  sendEmbed(embed: EmbedMessage): Promise<void>;
 };
