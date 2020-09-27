2d5d2660494be631e5360dac0a0f1ccc13464433
MikuroXina <ryosukadnak@gmail.com>
2020-09-27 16:21:33 +0900

    :sparkles: Add MainContent

--- src/web/pages/index.tsx
+++ src/web/pages/index.tsx
@@ -1,40 +1,52 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { FC } from 'react';
+
 import { Button } from '../components/button';
 
-const Main = ({pageSubTitle}: {pageSubTitle: string}) => <><main>
+interface MainProps { pageSubTitle: string }
+
+const MainContent: FC<MainProps> = ({pageSubTitle}: MainProps) =>
+  <>
     <h1>{pageSubTitle}</h1>
     <p>こちらのページからワンクリックで Discord 通知を登録できます。</p>
     <Button>通知を登録</Button>
     <footer>
       GitHub Secretary--Copyright (c) 2020 MikuroXina, all rights reserved.
     </footer>
-  </main>
-      <style jsx>{`
-        main {
-          margin: 0;
-          padding: 0;
-          width: 100%;
-          height: 100%;
-          display: flex;
-          flex-flow: column;
-          justify-content: center;
-          align-items: center;
-        }
-        h1 {
-          font-size: 1.8em;
-          color: #fdfdfd;
-        }
-        p {
-          color: #f7f7f7;
-          padding: 2rem;
-        }
-        footer {
-          position: fixed;
-          bottom: 1em;
-          text-align: center;
-        }
-      `}</style></>;
+  </>;
+
+const Main: FC<MainProps> = (props: MainProps) =>
+  <>
+    <main>
+      <MainContent {...props}/>
+    </main>
+    <style jsx>{`
+      main {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        align-items: center;
+      }
+      h1 {
+        font-size: 1.8em;
+        color: #fdfdfd;
+      }
+      p {
+        color: #f7f7f7;
+        padding: 2rem;
+      }
+      footer {
+        position: fixed;
+        bottom: 1em;
+        text-align: center;
+      }
+    `}</style>
+  </>;
 
 const home: NextPage = () => {
   const pageTitle = 'GitHub Secretary';
