97ef69420524691009188bf159095d5d25f7b927
MikuroXina <ryosukadnak@gmail.com>
2020-10-30 00:08:29 +0900

    🐛 Fix implementation

--- src/bot/skin/fauna-db.ts
+++ src/bot/skin/fauna-db.ts
@@ -1,5 +1,6 @@
 /* eslint-disable new-cap */
 
+import type { GitHubUser, GitHubUsers } from "../exp/github-user";
 import type {
   SubscriptionDatabase,
   UpdateHandler,
@@ -7,7 +8,6 @@ import type {
 } from "../op/interfaces";
 import fauna, { query as q } from "faunadb";
 import type { DiscordId } from "../exp/discord-id";
-import type { GitHubUser } from "../exp/github-user";
 import type { NotificationId } from "../exp/github-notification";
 
 const FAUNA_SECRET = process.env.FAUNA_SECRET || "UNSET";
@@ -21,24 +21,54 @@ export class FaunaDB implements SubscriptionDatabase, UserDatabase {
     this.#handlers.push(handler);
   }
 
-  update(id: DiscordId, notificationIds: NotificationId[]): Promise<void> {
-    return this.#client.query(
-      q.Replace(q.Collection("users", id), { notificationIds }),
+  private async notifyUpdate(): Promise<void> {
+    const { data } = (await this.#client.query(
+      q.Match(q.Index("all_users")),
+    )) as {
+      data: ({ id: DiscordId } & GitHubUser)[];
+    };
+    const users = data.reduce(
+      (prev, { id, ...rest }) => ({
+        ...prev,
+        [id]: { ...rest },
+      }),
+      {} as GitHubUsers,
     );
+
+    for (const handler of this.#handlers) {
+      handler.handleUpdate(users);
+    }
   }
 
-  register(id: DiscordId, user: GitHubUser): Promise<void> {
-    return this.#client.query(q.Create(q.Collection("users", id), user));
+  async update(
+    id: DiscordId,
+    notificationIds: NotificationId[],
+  ): Promise<void> {
+    await this.#client.query(
+      q.Update(q.Ref(q.Collection("users"), id), { notificationIds }),
+    );
+    await this.notifyUpdate();
   }
 
-  unregister(id: DiscordId): Promise<boolean> {
-    return this.#client.query(q.Delete(q.Collection("users", id)));
+  async register(id: DiscordId, user: GitHubUser): Promise<void> {
+    await this.#client.query(q.Create(q.Ref(q.Collection("users"), id), user));
+    await this.notifyUpdate();
+  }
+
+  async unregister(id: DiscordId): Promise<boolean> {
+    try {
+      await this.#client.query(q.Delete(q.Ref(q.Collection("users"), id)));
+    } catch (_err) {
+      return false;
+    }
+    await this.notifyUpdate();
+    return true;
   }
 
   async fetchUser(discordId: DiscordId): Promise<GitHubUser | undefined> {
     const { data } = (await this.#client.query(
-      q.Get(q.Collection("users", discordId)),
-    )) as { data: GitHubUser | undefined };
-    return data;
+      q.Match(q.Index("users_by_id", discordId)),
+    )) as { data: GitHubUser[] };
+    return data[0];
   }
 }
