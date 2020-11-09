d47b4c663d3e66106b3c11c05e3b39ceecb1c32b
MikuroXina <ryosukadnak@gmail.com>
2020-11-10 01:27:20 +0900

    🎨 Tidy interfaces up

---
+++ src/bot/abst/api.ts
@@ -0,0 +1,51 @@
+import type { Branch, PartialBranch } from "../exp/github/branch";
+import type { Issue, PartialIssue } from "../exp/github/issue";
+import type { PartialPullRequest, PullRequest } from "../exp/github/pr";
+import type { GitHubUser } from "../exp/github-user";
+import type { NotificationId } from "../exp/github-notification";
+import type { Repository } from "../exp/github/repo";
+
+export interface Api {
+  fetchRepo: (owner: string, repoName: string) => Promise<Repository>;
+}
+
+export interface IssueApi extends Api {
+  fetchIssues: (owner: string, repoName: string) => Promise<PartialIssue[]>;
+  fetchAnIssue: (
+    owner: string,
+    repoName: string,
+    dst: string,
+  ) => Promise<Issue>;
+}
+
+export interface PullApi extends Api {
+  fetchPullRequests: (
+    owner: string,
+    repoName: string,
+  ) => Promise<PartialPullRequest[]>;
+  fetchAPullRequest: (
+    owner: string,
+    repoName: string,
+    dst: string,
+  ) => Promise<PullRequest>;
+}
+
+export interface BranchApi extends Api {
+  fetchBranches: (owner: string, repoName: string) => Promise<PartialBranch[]>;
+  fetchABranch: (
+    owner: string,
+    repoName: string,
+    branchName: string,
+  ) => Promise<Branch>;
+}
+
+export interface NotificationApi {
+  markAsRead(
+    user: GitHubUser,
+    notificationId: NotificationId,
+  ): Promise<boolean>;
+}
+
+export interface UserApi {
+  getGitHubUser(userName: string, token: string): Promise<GitHubUser>;
+}
