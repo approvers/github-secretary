241f31019a01664fc41e8e1552dcba49ecca9a12
MikuroXina <ryosukadnak@gmail.com>
2021-08-02 21:48:19 +0900

    lint: Remove eslint-disable

--- src/bot/exp/slash-command.ts
+++ src/bot/exp/slash-command.ts
@@ -37,28 +37,20 @@ export type ApplicationCommandOptionType =
 const typeIntoRawValue = (self: ApplicationCommandOptionType): number => {
   switch (self) {
     case "SUB_COMMAND":
-      // eslint-disable-next-line no-magic-numbers
       return 1;
     case "SUB_COMMAND_GROUP":
-      // eslint-disable-next-line no-magic-numbers
       return 2;
     case "STRING":
-      // eslint-disable-next-line no-magic-numbers
       return 3;
     case "INTEGER":
-      // eslint-disable-next-line no-magic-numbers
       return 4;
     case "BOOLEAN":
-      // eslint-disable-next-line no-magic-numbers
       return 5;
     case "USER":
-      // eslint-disable-next-line no-magic-numbers
       return 6;
     case "CHANNEL":
-      // eslint-disable-next-line no-magic-numbers
       return 7;
     case "ROLE":
-      // eslint-disable-next-line no-magic-numbers
       return 8;
     default:
       throw new Error("unreachable");
