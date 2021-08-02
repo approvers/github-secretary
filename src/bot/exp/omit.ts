b8711787e9aefeda2192c02093d3a60199d90e4c
MikuroXina <ryosukadnak@gmail.com>
2021-08-02 21:44:06 +0900

    fix: Refactor omit

--- src/bot/exp/omit.ts
+++ src/bot/exp/omit.ts
@@ -4,5 +4,6 @@ export const omitBody = (body: string): string => {
   if (body.length < bodyWidth) {
     return body;
   }
-  return `${body.slice(0, bodyWidth)}...`;
+  const omitted = body.slice(0, bodyWidth);
+  return `${omitted}...`;
 };
