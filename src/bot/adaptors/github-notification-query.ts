9e9c7c03e05ded30885f2170af2e5c28a7098655
MikuroXina <ryosukadnak@gmail.com>
2021-10-24 17:53:04 +0900

    Fix incorrect handling

--- src/bot/adaptors/github-notification-query.ts
+++ src/bot/adaptors/github-notification-query.ts
@@ -1,7 +1,7 @@
-import fetch, { FetchError } from "node-fetch";
 import { GitHubNotifications } from "../model/github-notification";
 import { GitHubUser } from "../model/github-user";
 import { Query } from "../services/notify";
+import fetch from "node-fetch";
 
 export const notificationQuery: Query = {
   async fetchNotification({
@@ -11,21 +11,14 @@ export const notificationQuery: Query = {
     const base64 = Buffer.from(`${userName}:${notificationToken}`).toString(
       "base64",
     );
-    try {
-      const rawRes = await fetch("https://api.github.com/notifications", {
-        headers: {
-          Authorization: `Basic ${base64}`,
-        },
-      });
-      if (!rawRes.ok) {
-        return [];
-      }
-      return [...((await rawRes.json()) as unknown[])] as GitHubNotifications;
-    } catch (err: unknown) {
-      if (err instanceof FetchError) {
-        return [];
-      }
-      throw err;
+    const rawRes = await fetch("https://api.github.com/notifications", {
+      headers: {
+        Authorization: `Basic ${base64}`,
+      },
+    });
+    if (!rawRes.ok) {
+      return [];
     }
+    return [...((await rawRes.json()) as unknown[])] as GitHubNotifications;
   },
 };
